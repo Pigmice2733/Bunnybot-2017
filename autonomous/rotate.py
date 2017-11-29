ff11bf10a8838a4777af46b8ca192b14be859fe6
Orion Capell <orion.capell@gmail.com>
2017-11-28 16:56:22 -0800

    Update rotate.py

--- autonomous/rotate.py
+++ autonomous/rotate.py
@@ -9,5 +9,7 @@ class Rotate(AutonomousStateMachine):
 
     @state(first=True)
     def rotate(self):
-        if self.drivetrain.rotate(degrees=-90):
+        if self.drivetrain.rotate(degrees=-90):     
+            '''This tells us that if the robot rotates 90 degrees, the state is done.
+            it calls on the drivetrain and it's code for "rotate", then if it can run it, the state ends.'''
             self.done()
