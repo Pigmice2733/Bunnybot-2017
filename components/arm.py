d00e75716484d4df327b6c3dc3226669cfd93751
Caleb Eby <calebeby@users.noreply.github.com>
2017-12-07 15:47:48 -0800

    Fix arm speed (#26)

--- components/arm.py
+++ components/arm.py
@@ -29,5 +29,5 @@ class Arm:
                 motor_speed = 0.4
         else:
             if not self.retracted_limit_switch.get():
-                motor_speed = -0.2
+                motor_speed = -0.6
         self.arm_motor.set(motor_speed)
